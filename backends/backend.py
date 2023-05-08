b77934b216cc674c47b9a865958a154fcbbbbe44
jiri.one <nemec@jiri.one>
2023-05-08 12:07:53 +0200

    small change

--- backends/backend.py
+++ backends/backend.py
@@ -8,15 +8,15 @@ class DBBackend(ABC):
     def search_in_db(self, word, lang, fulltext) -> Iterator[str] | None:
         """The only mandatory method that provides a database search and that must return a result iterator or None."""
 
-    def search_sorted(self, word, lang, fulltext) -> list:
+    def search_sorted(self, word, lang, fulltext) -> list | str:
         results_with_matchratio = []
         results = self.search_in_db(word, lang, fulltext)
         if not results:
-            return ["No results found."]
+            return "No results found."
         for result in results:
             ratio = SequenceMatcher(None, result[lang], word).ratio()
             results_with_matchratio.append([result, ratio])
         sorted_results_with_ratio = sorted(
             results_with_matchratio, key=lambda x: x[1], reverse=True
         )
-        return [x for x, y in sorted_results_with_ratio]  # return list without ratio
+        return [x for x, y in sorted_results_with_ratio]  # return list of results without ratio
